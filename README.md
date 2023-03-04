0fd08f9efde5262d2bb94dd41429c4ef5fee2bdc
Seven Chord <hjlcarl@163.com>
2023-03-04 16:02:14 +0800

    fix: 优化颜色变量以及删除文档时的响应逻辑

--- README.md
+++ README.md
@@ -10,6 +10,32 @@
 - 在 `theme.js` 中引用入口文件 `calendar/main.js` 并运行 `calendarMain()` 函数
 - 修改 `calendar/main.js` 第36行，改为你当前 `calendar` 文件夹所在路径
 
+## 修改颜色
+
+将以下代码插入 `index.html` 的 `<head>` 标签内,并自行修改颜色
+
+```html
+<style>
+// 明亮模式的颜色即删除下行内的"[arco-theme='dark']"
+body[arco-theme='dark'] {
+    // 页面底色
+    --color-bg-1: #21252b !important;
+    // 下拉选择框输入时底色
+    --color-bg-2: #21252b !important;
+    // 下拉选择框底色 && 下拉菜单选项悬浮底色
+    --color-fill-2: #323842 !important;
+    --color-fill-3: #323842 !important;
+
+    // 日历底色
+    --color-bg-popup: #282c34 !important;
+    // 非当月日期，有日记底色（r,g,b）
+    --gray-1: 33, 37, 43 !important;
+    // 当月日期，有日记底色（r,g,b,a）
+    --arcoblue-1: 60, 126, 255, 0.2 !important;
+}
+</style>
+```
+
 ## 参考与感谢
 
 - [BryceAndJuly](https://github.com/BryceAndJuly) 在社区中的[首次实现](https://ld246.com/article/1662969146166)
--- src/Calendar.vue
+++ src/Calendar.vue
@@ -15,43 +15,6 @@
         </a-date-picker>
     </div>
 </template>
-<style lang="scss">
-.arco-picker-cell {
-    .arco-picker-date-value {
-        border: 1.4px solid transparent;
-        border-radius: var(--border-radius-medium);
-    }
-    &:not(.arco-picker-cell-selected) {
-        .arco-picker-date-value:hover {
-            background-color: transparent !important;
-        }
-        &.arco-picker-cell-in-view {
-            .arco-picker-date-value:hover {
-                border-color: rgb(var(--arcoblue-6));
-            }
-            .exist {
-                color: rgb(var(--arcoblue-6)) !important;
-                background-color: rgb(var(--arcoblue-1));
-                &:hover {
-                    background-color: rgb(var(--arcoblue-1)) !important;
-                }
-            }
-        }
-        &:not(.arco-picker-cell-in-view) {
-            .arco-picker-date-value:hover {
-                border-color: rgb(var(--gray-4));
-            }
-            .exist {
-                background-color: rgb(var(--gray-1));
-                &:hover {
-                    background-color: rgb(var(--gray-1)) !important;
-                }
-            }
-        }
-    }
-}
-</style>
-
 <script setup>
 import { request } from './utils';
 import { Socket } from './socket';
@@ -67,6 +30,12 @@ const existDailyNotes = ref([]);
 const dailyNoteSavePath = ref(null);
 // 日记模板存放路径
 const dailyNoteTemplatePath = ref(null);
+// 当前面板已存在日记的ID
+const existDailyNotesID = computed(() =>
+    existDailyNotes.value.map((dailyNote) => {
+        return dailyNote.id;
+    })
+);
 // 当前面板已存在日记的路径
 const existDailyNotesHpath = computed(() =>
     existDailyNotes.value.map((dailyNote) => {
@@ -75,10 +44,15 @@ const existDailyNotesHpath = computed(() =>
 );
 
 // 当前笔记本为空报错
-function notebookError() {
+function notebookIsNull() {
+    if (notebook.value) {
+        return false;
+    }
+    existDailyNotes.value = [];
     request('/api/notification/pushErrMsg', {
         msg: '[日历插件] 获取当前笔记本失败，请手动设置',
     });
+    return true;
 }
 
 // 月/日数字格式化
@@ -129,25 +103,7 @@ function getHpath(path, date = [year, month, day]) {
         ?.replaceAll('[[day]]', day);
 }
 
-watch(notebook, (newValue) => setCalendar(newValue), { deep: true });
-
-async function setCalendar(book) {
-    if (!book) {
-        existDailyNotes.value = [];
-        notebookError();
-        return;
-    }
-    // 获取含变量的日记存放路径
-    const bookId = book.value;
-    const data = await request('/api/notebook/getNotebookConf', { notebook: bookId });
-    dailyNoteSavePath.value = data.conf.dailyNoteSavePath.replace(
-        /{{.*?"(?<str>.*?)".*?}}/g,
-        '{{$<str>}}'
-    );
-    dailyNoteSavePath.value = parsePath(dailyNoteSavePath.value);
-    dailyNoteTemplatePath.value = data.conf.dailyNoteTemplatePath.replaceAll('/', '\\');
-
-    // 获取已存在的日记
+async function getExistDailyNotes(book) {
     let hpath = getHpath(dailyNoteSavePath.value, ['%', '%', '%']);
     const dailyNotes = await request('/api/query/sql', {
         stmt: `select * from blocks where type='d' and box = '${book.value}' and hpath like '${hpath}'`,
@@ -161,67 +117,155 @@ async function setCalendar(book) {
     existDailyNotes.value = tempExistDailyNotes;
 }
 
-// 设置cell样式
-function getCell(date) {
-    let hpath = getHpath(dailyNoteSavePath.value, deconstructDate(date));
-    return existDailyNotesHpath.value.includes(hpath);
+watch(notebook, (newValue) => setCalendar(newValue), { deep: true });
+async function setCalendar(book) {
+    if (notebookIsNull()) {
+        return;
+    }
+    // 获取含变量的日记存放路径
+    const bookID = book.value;
+    const data = await request('/api/notebook/getNotebookConf', { notebook: bookID });
+    dailyNoteSavePath.value = data.conf.dailyNoteSavePath.replace(
+        /{{.*?"(?<str>.*?)".*?}}/g,
+        '{{$<str>}}'
+    );
+    dailyNoteSavePath.value = parsePath(dailyNoteSavePath.value);
+    dailyNoteTemplatePath.value = data.conf.dailyNoteTemplatePath.replaceAll('/', '\\');
+
+    // 获取已存在的日记
+    getExistDailyNotes(book);
+}
+
+// 重置日记数据
+const ws = new Socket();
+ws.on('removeDoc', resetExistDailyNote);
+
+function resetExistDailyNote(data) {
+    if (notebookIsNull()) {
+        return;
+    }
+    // 删除日记
+    const removeDocID = data.ids[0];
+    if (existDailyNotesID.value.includes(removeDocID)) {
+        existDailyNotes.value = existDailyNotes.value.filter((dailyNote) => {
+            return dailyNote.id !== removeDocID;
+        });
+        return;
+    }
+    // 删除日记祖先文档
+    setTimeout(() => {
+        getExistDailyNotes(notebook.value);
+    }, 3000);
 }
 
 // 创建日记
 async function createDailyNote(date) {
-    if (!notebook.value) {
-        notebookError();
+    if (notebookIsNull()) {
         return;
     }
-    let bookId = notebook.value.value;
+    let bookID = notebook.value.value;
     let hpath = getHpath(dailyNoteSavePath.value, deconstructDate(date));
 
+    // 当前日期已有日记，打开日记
     if (existDailyNotesHpath.value.includes(hpath)) {
-        // 打开日记
         let docID = existDailyNotes.value.find((dailyNote) => {
             return dailyNote.hpath === hpath;
         }).id;
         window.open(`siyuan://blocks/${docID}`);
-    } else {
-        // 创建日记
-        const docID = await request('/api/filetree/createDocWithMd', {
-            notebook: bookId,
-            path: hpath,
-            markdown: '',
-        });
-        existDailyNotes.value.push({ id: docID, hpath: hpath });
-        window.open(`siyuan://blocks/${docID}`);
+        return;
+    }
+    // 当前日期无日记，创建日记
+    const docID = await request('/api/filetree/createDocWithMd', {
+        notebook: bookID,
+        path: hpath,
+        markdown: '',
+    });
+    existDailyNotes.value.push({ id: docID, hpath: hpath });
+    window.open(`siyuan://blocks/${docID}`);
 
-        // 根据模板渲染笔记
-        if (dailyNoteTemplatePath.value.length) {
-            const system = await request('/api/system/getConf');
-            let templateDir =
-                system.conf.system.dataDir + '\\templates' + dailyNoteTemplatePath.value;
-            const render = await request('/api/template/render', {
-                id: docID,
-                path: templateDir,
-            });
-            request('/api/block/prependBlock', {
-                data: render.content,
-                dataType: 'dom',
-                parentID: docID,
-            });
-        }
+    // 根据模板渲染日记
+    if (dailyNoteTemplatePath.value.length) {
+        const system = await request('/api/system/getConf');
+        let templateDir = system.conf.system.dataDir + '\\templates' + dailyNoteTemplatePath.value;
+        const render = await request('/api/template/render', {
+            id: docID,
+            path: templateDir,
+        });
+        request('/api/block/prependBlock', {
+            data: render.content,
+            dataType: 'dom',
+            parentID: docID,
+        });
     }
 }
 
-const ws = new Socket();
-ws.on('removeDoc', resetExistDailyNote);
-
-// 重置日记数据
-async function resetExistDailyNote() {
-    let temp = [];
-    for (const dailyNote of existDailyNotes.value) {
-        const data = await request('/api/filetree/getHPathByID', { id: dailyNote.id });
-        if (data) {
-            temp.push(dailyNote);
-        }
-    }
-    existDailyNotes.value = temp;
+// 设置 cell 类
+function getCell(date) {
+    let hpath = getHpath(dailyNoteSavePath.value, deconstructDate(date));
+    return existDailyNotesHpath.value.includes(hpath);
 }
 </script>
+<style lang="less">
+body[arco-theme='dark'] {
+    // 页面底色
+    --color-bg-1: #21252b !important;
+    // 下拉选择框输入时底色
+    --color-bg-2: #21252b !important;
+    // 下拉选择框底色 && 下拉菜单选项悬浮底色
+    --color-fill-2: #323842 !important;
+    --color-fill-3: #323842 !important;
+
+    // 日历底色
+    --color-bg-popup: #282c34 !important;
+    // 非当月日期，有日记底色（r,g,b）
+    --gray-1: 33, 37, 43 !important;
+    // 当月日期，有日记底色（r,g,b,a）
+    --arcoblue-1: 60, 126, 255, 0.2 !important;
+}
+
+.arco-picker-cell {
+    .arco-picker-date-value {
+        border: 1.4px solid transparent;
+        border-radius: var(--border-radius-medium);
+    }
+    // 非选中日期
+    &:not(.arco-picker-cell-selected) {
+        .arco-picker-date-value:hover {
+            background-color: transparent !important;
+        }
+        // 当月日期
+        &.arco-picker-cell-in-view {
+            .arco-picker-date-value:hover {
+                border-color: rgb(var(--arcoblue-6));
+            }
+            .exist {
+                color: rgb(var(--arcoblue-6)) !important;
+                background-color: rgb(var(--arcoblue-1));
+                &:hover {
+                    background-color: rgb(var(--arcoblue-1)) !important;
+                }
+            }
+        }
+        // 非当月日期
+        &:not(.arco-picker-cell-in-view) {
+            .arco-picker-date-value:hover {
+                border-color: rgb(var(--gray-4));
+            }
+            .exist {
+                background-color: rgb(var(--gray-1));
+                &:hover {
+                    background-color: rgb(var(--gray-1)) !important;
+                }
+            }
+        }
+    }
+}
+
+.arco-select-dropdown {
+    padding: 6px !important;
+}
+
+.arco-select-option {
+    border-radius: var(--border-radius-medium);
+}
+</style>
