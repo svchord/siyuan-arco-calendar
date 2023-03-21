a0f70d1204491ef54683383e1d6e364e8cbbbc95
zuoez02 <zuoez02@outlook.com>
2023-03-21 07:25:23 +0000

    fix: Style inject

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ArcoResolver } from 'unplugin-vue-components/resolvers'
-
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        AutoImport({
-            resolvers: [ArcoResolver()]
-        }),
-        Components({
-            resolvers: [
-                ArcoResolver({
-                    sideEffect: true
-                })
-            ]
-        }),
         cssInjectedByJsPlugin()
     ],
     resolve: {
